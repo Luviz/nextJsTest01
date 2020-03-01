5345e45f07c4ef5c59e50e3a06705648730f3cfc
Barida Jedi <Bardiajedi@gmail.com>
2020-03-01 07:47:02 +0000

    added secret stuff

--- pages/aboutMe.js
+++ pages/aboutMe.js
@@ -1,13 +1,16 @@
 import Layout from "../components/masterpages";
 import css from "../styles.scss"
 // import Image from "next/types"
-
-
+const now = require('./my.json');
+console.log(now);
 const AboutMe = props => <Layout>
     <div className={css.aboutMe}>
         <div>
             <img className={css.myphoto} src="/img.jpg" height={300} alt="my image" />
         </div>
+        <h2>
+            {JSON.stringify(now)}
+        </h2>
     </div>
     
 </Layout>
